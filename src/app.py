f96c52c49636bb38564bb49f22506bd2f266e373
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-13 18:57:56 +0530

    tejas

---
+++ src/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+
+
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")

95a6b141550af1a6a33b82cb056253646b9342a7
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-16 23:35:16 +0530

    tekas

--- src/app.py
+++ src/app.py
@@ -66,6 +66,7 @@ def get_response(user_input):
     })
     
     return response['answer']
+
 # app config
 st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 st.title("Chat with websites")
